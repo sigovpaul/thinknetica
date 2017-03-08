ca8e7fa77a4808e5cff381ca206e469cd4d73503
Sigov Paul <sigovpaul@gmail.com>
2017-03-08 17:05:47 +0300

    lesson 1

--- lesson_1/equation.rb
+++ lesson_1/equation.rb
@@ -6,15 +6,16 @@ b = gets.chomp.to_f
 puts "Введите  c:"
 c = gets.chomp.to_f
 d = ( b ** 2 - 4 * a * c )
-if d > 0
-  x1 = ( - b + c ) / ( 2 * a )
-  x2 = ( - b - c ) / ( 2 * a )
-  puts "Уравнение имеет 2 корня: x1 = #{x1}, x2 = #{x2}"
+if d < 0
+  puts "Уравнение не имеет корней"
 elsif d == 0
   x1 = x2 = ( - b ) / 2 * a
   puts "Уравнение имеет равные корни: x1 = x1 = #{x1}"
 else
-  puts "Уравнение не имеет корней"
+  d_sqrt = Math.sqrt(d)
+  x1 = ( - b + d_sqrt ) / ( 2 * a )
+  x2 = ( - b - d_sqrt ) / ( 2 * a )
+  puts "Уравнение имеет 2 корня: x1 = #{x1}, x2 = #{x2}"
 end
 
   
