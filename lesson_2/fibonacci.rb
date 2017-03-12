5f4a1e5487cef4169113c69c010393ca71a45c5a
Sigov Paul <sigovpaul@gmail.com>
2017-03-12 22:55:46 +0300

    lesson 2

---
+++ lesson_2/fibonacci.rb
@@ -0,0 +1,12 @@
+#3. Заполнить массив числами фибоначи до 100
+array = [0,1]
+puts "Введите число, при котором запись массива должна быть остановлена:"
+a = gets.chomp.to_f
+i = 2
+loop do 
+  new_element = array[i - 1] + array[i - 2]
+break if new_element > a
+  array << (new_element)
+  i += 1
+end
+puts array 
