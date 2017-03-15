2bc2307fd36afe680dbadb94304fb6e765a619da
Sigov Paul <sigovpaul@gmail.com>
2017-03-15 23:17:06 +0300

    lesson 2 fix

--- lesson_2/date_of_the_year.rb
+++ lesson_2/date_of_the_year.rb
@@ -3,9 +3,9 @@ day = gets.to_i
 puts "Введите месяц"
 month = gets.chomp.to_i
 until (0..12).include? month do
-	puts "Неверное значение месяца"
-	puts "Введите месяц"
-	month = gets.to_i
+  puts "Неверное значение месяца"
+  puts "Введите месяц"
+  month = gets.to_i
 end
 
 puts "Введите год"
@@ -13,16 +13,16 @@ year = gets.to_i
 days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
 if year % 4 == 0 && year % 100 != 0 || year % 400 == 0
-	puts "Год високосный"
-	days[1] = 29
+  puts "Год високосный"
+  days[1] = 29
 else
-	puts "Год невисокосный"
+  puts "Год невисокосный"
 end
 
 count = 0
 
 for i in 0..month-2
-	count += days[i]
+  count += days[i]
 end
 
 date = count + day
