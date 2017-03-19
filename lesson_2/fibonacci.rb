9c69355ea3c3e89f5f57a13d30c9d82dcf3d9da6
Sigov Paul <sigovpaul@gmail.com>
2017-03-19 23:49:47 +0300

    loop fix

--- lesson_2/fibonacci.rb
+++ lesson_2/fibonacci.rb
@@ -2,10 +2,10 @@
 array = [0,1]
 puts "Введите число, при котором запись массива должна быть остановлена:"
 a = gets.chomp.to_f
-#i = 1
-until array.last < a do
-  new_element = array.last + array[ - 2]
-  array << new_element
-  puts array
+i = 1
+while i < a do
+  array << i
+  i = array.last + array[ - 2]
 end
+puts array
  
